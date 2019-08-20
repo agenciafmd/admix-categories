06ba703eb0ce61b5a730af9d60deae1e8606c91f
Irineu Junior <irineu@fmd.ag>
2019-08-20 14:09:53 -0300

    fix(packages): adiciona o name nas partials

--- src/resources/views/partials/form/filter.blade.php
+++ src/resources/views/partials/form/filter.blade.php
@@ -2,7 +2,7 @@
 
 <h6 class="dropdown-header bg-gray-lightest p-2">{{ config("admix-categories.{$type}.name") }}</h6>
 <div class="p-2">
-    {{ Form::select('filter[' . Str::singular($type) . "_id" . ']', ['' => '-'] + $categoryService->lists($type)->toArray(), filter(Str::singular($type) . "_id"), [
+    {{ Form::select('filter[' . $name . "_id" . ']', ['' => '-'] + $categoryService->lists($type)->toArray(), filter($name . "_id"), [
             'class' => 'form-control form-control-sm'
         ]) }}
 </div>
--- src/resources/views/partials/form/select.blade.php
+++ src/resources/views/partials/form/select.blade.php
@@ -1,3 +1,3 @@
 @inject('categoryService', '\Agenciafmd\Categories\Services\CategoryService')
 
-{!! Form::bsSelect(config("admix-categories.{$type}.name"), Str::singular($type) . "_id", ['' => '-'] + $categoryService->lists($type)->toArray(), null, ['required' => ($required) ?? false ]) !!}
+{!! Form::bsSelect(config("admix-categories.{$type}.name"), $name . "_id", ['' => '-'] + $categoryService->lists($type)->toArray(), null, ['required' => ($required) ?? false ]) !!}
