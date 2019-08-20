3f5181dec9c6028da798372148221ad074a7257d
Irineu Junior <irineu@fmd.ag>
2019-08-20 14:13:10 -0300

    fix(packages): adiciona o name nas partials

--- src/resources/views/partials/form/filter.blade.php
+++ src/resources/views/partials/form/filter.blade.php
@@ -2,7 +2,7 @@
 
 <h6 class="dropdown-header bg-gray-lightest p-2">{{ config("admix-categories.{$type}.name") }}</h6>
 <div class="p-2">
-    {{ Form::select('filter[' . $name . "_id" . ']', ['' => '-'] + $categoryService->lists($type)->toArray(), filter($name . "_id"), [
+    {{ Form::select('filter[' . $name . ']', ['' => '-'] + $categoryService->lists($type)->toArray(), filter($name), [
             'class' => 'form-control form-control-sm'
         ]) }}
 </div>
