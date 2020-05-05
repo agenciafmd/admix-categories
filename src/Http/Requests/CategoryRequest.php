9c1b0c0ae24f0bb3dd657b8403ba1944743fcb16
Irineu Junior <irineu@fmd.ag>
2020-05-05 15:41:55 -0300

    feat(request): corrige errorBag

--- src/Http/Requests/CategoryRequest.php
+++ src/Http/Requests/CategoryRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class CategoryRequest extends FormRequest
 {
+    protected $errorBag = 'admix';
+
     public function rules()
     {
         return [
